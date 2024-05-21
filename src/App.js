b83065d2197e26767c7b222f9b1c5e7cacd0efc0
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2024-05-21 19:59:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * 2997 )]
 
-    window.location = `https://www.google.com/search?q=site%3Areddemy.com+${randSearch}&rlz=1C1FKPE_enIN956IN956&oq=site%3Areddemy.com+a&aqs=chrome.0.69i59l2j69i58.4336j0j15&sourceid=chrome&ie=UTF-8`
+    window.location = `https://www.google.com/search?q=site%3Aredditfavorites.com%2Fservices+${randSearch}&sca_esv=fdd505efaadfc2c0&sca_upv=1&ei=O69MZpWYHfzjseMP6ca0yAw&ved=0ahUKEwjVodXm-Z6GAxX8cWwGHWkjDckQ4dUDCBA&uact=5&oq=site%3Aredditfavorites.com%2Fservices+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiI3NpdGU6cmVkZGl0ZmF2b3JpdGVzLmNvbS9zZXJ2aWNlcyBhSM4JUK0IWK0IcAR4AJABAJgBAKABAKoBALgBA8gBAPgBAZgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
   }
   return (
     <div className='App' onClick={handleClick}>
